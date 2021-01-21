ebfa919aab3f27b2e40012852b91f6a934d056df
Mo <mcresp495@gmail.com>
2021-01-21 19:01:30 +0200

    create the newyear app

--- isItChristmas/settings.py
+++ isItChristmas/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'is_it_christmas',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ is_it_christmas/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IsItChristmasConfig(AppConfig):
+    name = 'is_it_christmas'
